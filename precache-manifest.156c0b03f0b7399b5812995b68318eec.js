2b19bc28fa6947b2d3ea7cdb29bd412aef5b2145
Egor Vladimirov <wladimirow.es@gmail.com>
2020-06-03 12:18:51 +0300

    Upload dist

---
+++ precache-manifest.156c0b03f0b7399b5812995b68318eec.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec56ae84eeaf2353dbad",
+    "url": "/css/app.9b533743.css"
+  },
+  {
+    "revision": "a52453d357bff57d9c7b",
+    "url": "/css/chunk-2eeab0e2.13ffceb5.css"
+  },
+  {
+    "revision": "7417e695572073db38d0",
+    "url": "/css/chunk-475078fc.2bfbb72e.css"
+  },
+  {
+    "revision": "47e1d3f0d405ceb1ff413ce21e7f41b1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ec56ae84eeaf2353dbad",
+    "url": "/js/app.4158876f.js"
+  },
+  {
+    "revision": "a52453d357bff57d9c7b",
+    "url": "/js/chunk-2eeab0e2.14ab7e97.js"
+  },
+  {
+    "revision": "7417e695572073db38d0",
+    "url": "/js/chunk-475078fc.7a76ae95.js"
+  },
+  {
+    "revision": "0c3291ac37880498764f",
+    "url": "/js/chunk-vendors.58440b4e.js"
+  },
+  {
+    "revision": "5b00ad3f4d56c8e310b9bae2cfc1f444",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
